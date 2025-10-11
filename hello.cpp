cc8e937f08472ce8695d1ce99cc33343ba4faf76
Uoc <vanssenlingga20@gmail.com>
2025-10-11 22:48:28 +0000

    add hello.cpp in directory commands

---
+++ hello.cpp
@@ -0,0 +1,23 @@
+#include "include/command.hpp"
+#include <dpp/dpp.h>
+
+// Example command with one string option
+static bool _hello_registered = []() {
+    register_command([](dpp::cluster& bot){
+        dpp::command_option name_opt(dpp::co_string, "name", "Your name", true);
+        dpp::slashcommand hello_cmd("hello", "Say hello", { name_opt });
+
+        bot.global_command_create(hello_cmd);
+
+        bot.on_slashcommand([](const dpp::slashcommand_t& event){
+            if (event.command.get_name() == "hello") {
+                std::string name = "there";
+                if (!event.get_parameter("name").empty()) {
+                    name = event.get_parameter("name");
+                }
+                event.reply("Hello, " + name + "!");
+            }
+        });
+    });
+    return true;
+}();
