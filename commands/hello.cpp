4d450757824fcd85727549993a2e37de6e77b118
Uoc <vanssenlingga20@gmail.com>
2025-10-11 23:25:14 +0000

    update hello.cpp command now using DPP API version 10

--- commands/hello.cpp
+++ commands/hello.cpp
@@ -1,23 +1,33 @@
 #include "command.hpp"
 #include <dpp/dpp.h>
+#include <iostream>
 
-// Example command with one string option
-static bool _hello_registered = []() {
-    register_command([](dpp::cluster& bot){
-        dpp::command_option name_opt(dpp::co_string, "name", "Your name", true);
-        dpp::slashcommand hello_cmd("hello", "Say hello", { name_opt });
+void register_hello_command(dpp::cluster& bot) {
+    // Ambil application ID dari bot
+    dpp::snowflake app_id = bot.me.id;
 
-        bot.global_command_create(hello_cmd);
+    // Buat slash command
+    dpp::slashcommand hello_cmd("hello", "Say hello", app_id);
 
-        bot.on_slashcommand([](const dpp::slashcommand_t& event){
-            if (event.command.get_name() == "hello") {
-                std::string name = "there";
-                if (!event.get_parameter("name").empty()) {
-                    name = event.get_parameter("name");
-                }
-                event.reply("Hello, " + name + "!");
+    // Tambahkan optional argument
+    hello_cmd.add_option(
+        dpp::command_option(dpp::co_string, "name", "Your name", false)
+    );
+
+    // Register ke Discord
+    bot.global_command_create(hello_cmd);
+
+    // Event handler untuk respon command
+    bot.on_slashcommand([](const dpp::slashcommand_t& event) {
+        if (event.command.get_command_name() == "hello") {
+            std::string name = "there";
+
+            auto param = event.get_parameter("name");
+            if (std::holds_alternative<std::string>(param)) {
+                name = std::get<std::string>(param);
             }
-        });
+
+            event.reply("Hello, " + name + "!");
+        }
     });
-    return true;
-}();
+}
