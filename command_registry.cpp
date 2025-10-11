f9692840d6c2af5d11785f572480f0835ab66e15
Uoc <vanssenlingga20@gmail.com>
2025-10-11 22:46:34 +0000

    add command_registry.cpp in root directory

---
+++ command_registry.cpp
@@ -0,0 +1,18 @@
+#include "include/command.hpp"
+#include <vector>
+#include <mutex>
+
+static std::vector<CommandRegistrar>& registry() {
+    static std::vector<CommandRegistrar> r;
+    return r;
+}
+
+void register_command(const CommandRegistrar& r) {
+    registry().push_back(r);
+}
+
+void register_all_commands(dpp::cluster& bot) {
+    for (auto &r : registry()) {
+        r(bot);
+    }
+}
